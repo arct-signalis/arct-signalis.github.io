e31b622b6a7035472dd3cabe4c92f55551777dec
Hladikes <adi.hladik@gmail.com>
2025-11-09 15:34:30 +0100

    deploy fix

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,38 +1,50 @@
 <script lang="ts">
   import { page } from '$app/stores'
+  import { resolve } from '$app/paths'
 </script>
 
-<nav class="px-3 border-b border-b-primary/10 bg-background sticky top-0 z-50 flex justify-between">
+<nav
+  class="px-3 border-b border-b-primary/10 bg-background sticky top-0 z-50 flex justify-between"
+>
   <div class="flex space-x-6">
-    <a 
-      href="/" 
+    <a
+      href={resolve('/')}
       class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4"
-      class:underline={$page.url.pathname === '/'}>Domov</a>
+      class:underline={$page.url.pathname === '/'}>Domov</a
+    >
 
-    <a 
-      href="/posts" 
+    <a
+      href={resolve('/posts')}
       class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4"
-      class:underline={$page.url.pathname.includes('/posts')}>Zápisnice</a>
-      
-     <a 
-      href="https://prolaborate.systemthinking.sk/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%3Fclient_id%3DProlaborate-spa%26redirect_uri%3Dhttps%253A%252F%252Fprolaborate.systemthinking.sk%252Fsignin-callback%26response_type%3Dcode%26scope%3Dprofile%2520openid%2520offline_access%26state%3D506b636809b048929d6e8576f2dad470%26code_challenge%3D1EP9fUpfRDH9QJPjmbE-jqFDrxEZZXErjnui_kll7jY%26code_challenge_method%3DS256%26response_mode%3Dquery%26redirecturl%3D%252F" 
-      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4">
-      Prolaborate</a>
+      class:underline={$page.url.pathname.includes('/posts')}>Zápisnice</a
+    >
 
-     <a 
-      href="https://www.youtube.com/channel/UCkk9TIfPDi27vWxdbKLPEPg" 
-      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4">
-      Youtube</a>
+    <a
+      href="https://prolaborate.systemthinking.sk/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%3Fclient_id%3DProlaborate-spa%26redirect_uri%3Dhttps%253A%252F%252Fprolaborate.systemthinking.sk%252Fsignin-callback%26response_type%3Dcode%26scope%3Dprofile%2520openid%2520offline_access%26state%3D506b636809b048929d6e8576f2dad470%26code_challenge%3D1EP9fUpfRDH9QJPjmbE-jqFDrxEZZXErjnui_kll7jY%26code_challenge_method%3DS256%26response_mode%3Dquery%26redirecturl%3D%252F"
+      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4"
+    >
+      Prolaborate</a
+    >
 
-     <a 
-      href="https://github.com/arct-signalis" 
-      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4">
-      Github</a>
+    <a
+      href="https://www.youtube.com/channel/UCkk9TIfPDi27vWxdbKLPEPg"
+      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4"
+    >
+      Youtube</a
+    >
 
-    <a 
-      href="https://www.linkedin.com/company/system-thinking-in-it-and-digital-fabrication/" 
-      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4">
-      Linkedin</a>
+    <a
+      href="https://github.com/arct-signalis"
+      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4"
+    >
+      Github</a
+    >
+
+    <a
+      href="https://www.linkedin.com/company/system-thinking-in-it-and-digital-fabrication/"
+      class="flex items-center py-4 font-medium text-accent hover:underline underline-offset-4"
+    >
+      Linkedin</a
+    >
   </div>
-
 </nav>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import type { PageServerData } from "./$types"
+  import { resolve } from '$app/paths'
+  import type { PageServerData } from './$types'
 
   export let data: PageServerData
 </script>
@@ -12,12 +13,15 @@
 
 <div class="flex flex-col space-y-3">
   {#each data.posts as post}
-    <a 
-      href={`/posts/${post.slug}`} 
-      class="space-y-3 flex flex-col rounded-lg py-5 px-5 border-2 border-transparent bg-primary/5 hover:border-accent hover:bg-primary/10">
+    <a
+      href={resolve(`/posts/${post.slug}`)}
+      class="space-y-3 flex flex-col rounded-lg py-5 px-5 border-2 border-transparent bg-primary/5 hover:border-accent hover:bg-primary/10"
+    >
       <h1 class="text-xl text-accent">{post.meta.title}</h1>
       {#if post.meta.description}
-        <h1 class="font-medium text-sm text-primary/90 whitespace-pre">{post.meta.description}</h1>
+        <h1 class="font-medium text-sm text-primary/90 whitespace-pre">
+          {post.meta.description}
+        </h1>
       {/if}
     </a>
   {/each}
